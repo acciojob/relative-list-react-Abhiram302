a42283f8abaecffb14c6c78e08208a1534ac0742
Abhiram302 <abhiramburra3@gmail.com>
2025-10-17 10:38:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,13 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+      <ol key = "relativeList>
+      <li key = "relativeListItem1>Abhiram</li>
+      <li key = "relativeListItem2>Dolly</li>
+      <li key = "relativeListItem3>hasini</li>
+      <li key = "relativeListItem4>Yogi</li>
+      <li key = "relativeListItem5>Sujatha</li>
+    </ol>
     </div>
   )
 }
